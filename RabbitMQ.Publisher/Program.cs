f4443c5fdbab943f8ce4cd5befa701cfcc287e62
RAHIME KIR <RAHIME.KIR@lcwaikiki.com>
2023-08-04 11:54:03 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+
+            // Factory üzerinden bağlantı açma
+            using var connection = factory.CreateConnection();
+
+            // Bu bağlantıya  rabbitmq'ya kanal üzerinden bağlanma
+            var channel = connection.CreateModel();  // kanal oluşturma
+
+            // mesajların gidebilmesi için kuyruk oluşturmak gerekiyor 
+            //QueueDeclare(string queue,bool durable, bool exclusive ,bool autoDelete,IDictionary<string,object> arguments)
+
+            //kuyruk ismi 
+
+            //durable :false => rabbit mqda olusan kuyruklar  memoryde tutulur. rabbit mq restart atarsa tüm kuyruklar silinir 
+            //durable :true => kuyruklar hiç bi zaman kaybolmaz. fiziksel olarak tutulur. (önerilen)
+
+            //exclusive: true => bu kuyruğa sadece burada oluşturulan kanal üzerinden bağlanılabilir.
+            //Subscriber tarafınan başka bir kanal üzerinden bağlanmak için false yapılmalıdır.
+
+            //autoDelete :true => eğer kuyruğa bağlı olan son subscriber da bağlantısını koparırısa otomatik olarak kuyruk silinir. (önerilmez)
+            //çünkü subscriber yanlışlıkla down olursa kuyruk silinir. kuyruk her zaman ayakta durmalı. 
+
+            channel.QueueDeclare("hello-queue",true,false,false);
+
+            string message = "hello queen ";  // bu stringi rabbit mq ya göndereceğiz
+                                              //rabbit mq ya mesajları byte dizini olarak gönderiririz. bu sayede istediğimiz her şeyi gönderebiliriz (pdj , image,büyük dosyalar vs)
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            //BasicPublish(string exchange, string routingKey, IBasicProporties basicProporties,ReadonlyMemory<byte>)
+            //şuan exchange kullanmıyoruz 
+            //routingKey kuyruğun ismi verilir. bu sayede bu roadmape e  göre gelen mesajı istenilen kuyruğa gönderebilsin.
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Mesaj gönderilmiştir.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+
+            // factory üzerinden bağlantı açma
+            using var connection = factory.CreateConnection();
+
+            // bağlantıya  rabbitmq'ya kanal üzerinden bağlanma
+            var channel = connection.CreateModel();  // kanal oluşturma
+
+            //ister kullan ister sil. silersek subscriberı ayağa kaldırdığımızda yoksa hata verir.
+            //publisher bu kuyruğu oluşturuyorsa gerek yok. 
+            // channel.QueueDeclare("hello-queue", true, false, false);
+
+            // nesnemizi oluşturduk.
+            var subscriber = new EventingBasicConsumer(channel);
+
+            //subscriberın dinleyeceği kuyrulu belirtme işlemi
+            //BasicConsume(string queue, bool autoAck)
+
+            //autoAck=> true : Mesaj doğru da işlense yanlış da işlense kuyruktan mesajı siler.
+            //autoAck=> false :Rabbit mq kuyruktan silmez. mesajı doğru bir sekilde işlenirse haber verip sildirir.
+            channel.BasicConsume("hello-queue", true, subscriber);
+
+
+            //Artık event üzerinden mesaj dinlenebilir.
+            // bu event (Received) , rabbit mq subscribera mesaj gönderdiğinde fırlayacak.
+            subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());  // mesajı alma 
+                Console.WriteLine("Gelen mesaj : " + message);
+            };
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
